ed98b6eedad8821f07c1ba72d70078e82e05f067
kherma <kamil.herma01@gmail.com>
2021-02-18 14:20:14 +0100

    Test 1. Render HappyHourAd

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types'
+
+const HappyHourAd = () => {
+  return <div>HappyHour</div>;
+};
+
+// HappyHourAd.propTypes = {
+// }
+
+export default HappyHourAd;
