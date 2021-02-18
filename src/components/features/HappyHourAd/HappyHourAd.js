bf87784f26baf78c80a65765d797c114c4939134
kherma <kamil.herma01@gmail.com>
2021-02-18 15:25:07 +0100

    Test 2. Render HappyHourAd elements

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -2,7 +2,12 @@ import React from 'react';
 // import PropTypes from 'prop-types'
 
 const HappyHourAd = () => {
-  return <div>HappyHour</div>;
+  return (
+    <div>
+      <h3 className="title">Happy Hour</h3>
+      <div className="countdown">countdown</div>
+    </div>
+  );
 };
 
 // HappyHourAd.propTypes = {
--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  countdown: '.countdown',
+};
+
 describe('Compoennt HappyHourAd', () => {
   it('Should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
+  });
+  it('Should contains title and countdown elements', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component.exists(select.title)).toBeTruthy();
+    expect(component.exists(select.countdown)).toBeTruthy();
   });
 });
