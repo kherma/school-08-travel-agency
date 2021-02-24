bc3272c71f3db4aa83e4773efe237708ec3814aa
kherma <kamil.herma01@gmail.com>
2021-02-24 17:33:15 +0100

    Vary phone numbers based on time

--- src/components/features/PhoneNumber/PhoneNumber.js
+++ src/components/features/PhoneNumber/PhoneNumber.js
@@ -2,7 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PhoneNumber = ({ phones }) => {
-  return <span>{phones[0].phone}</span>;
+  const setPhoneNumber = () => {
+    // Get curren UTC Hour
+    const currentHour = new Date().getUTCHours();
+
+    // Get number from state
+    for (const shift of phones) {
+      if (shift.start <= currentHour && shift.end >= currentHour) {
+        return shift.phone;
+      } else if (currentHour >= 22 || currentHour < 8) {
+        return phones[phones.length - 1].phone;
+      }
+    }
+  };
+  return <span>{setPhoneNumber()}</span>;
 };
 
 PhoneNumber.propTypes = {
--- src/components/features/PhoneNumber/PhoneNumber.test.js
+++ src/components/features/PhoneNumber/PhoneNumber.test.js
@@ -3,7 +3,14 @@ import { shallow } from 'enzyme';
 import PhoneNumber from './PhoneNumber';
 
 const mockProps = [
-  { start: 8, end: 12, name: 'Amanda', phone: '678.243.8455' },
+  { start: 8, end: 11, phone: '678.243.8455' },
+  { start: 12, end: 15, phone: '278.443.6443' },
+  { start: 16, end: 21, phone: '167.280.3970' },
+  {
+    start: 22,
+    end: 8,
+    phone: 'The office opens at 8:00 UTC',
+  },
 ];
 
 describe('Component PhoneNumber', () => {
@@ -11,4 +18,35 @@ describe('Component PhoneNumber', () => {
     const component = shallow(<PhoneNumber phones={mockProps} />);
     expect(component).toBeTruthy();
   });
+});
+
+const trueDate = Date;
+const mockDate = (customDate) =>
+  class extends Date {
+    constructor() {
+      super(customDate);
+    }
+  };
+
+const checkNumberAtTime = (time, expectedNumber) => {
+  it(`should show correct at ${time}`, () => {
+    global.Date = mockDate(`2021-02-24T${time}.135Z`);
+
+    const component = shallow(<PhoneNumber phones={mockProps} />);
+    const renderedNumber = component.find('span').text();
+    expect(renderedNumber).toEqual(expectedNumber);
+
+    global.Date = trueDate;
+  });
+};
+
+describe('Component HappyHourAd with mocked Date', () => {
+  checkNumberAtTime('08:00:00', mockProps[0].phone);
+  checkNumberAtTime('11:59:59', mockProps[0].phone);
+  checkNumberAtTime('12:00:00', mockProps[1].phone);
+  checkNumberAtTime('15:59:59', mockProps[1].phone);
+  checkNumberAtTime('16:00:00', mockProps[2].phone);
+  checkNumberAtTime('21:59:59', mockProps[2].phone);
+  checkNumberAtTime('22:00:00', mockProps[3].phone);
+  checkNumberAtTime('07:59:59', mockProps[3].phone);
 });
